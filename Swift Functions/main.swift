26883b02b65a189cc19beb7a23888e11e8cd329f
Paul Orton <ortonpaul18@gmail.com>
2019-02-11 12:10:05 -0600

    First commit

---
+++ Swift Functions/main.swift
@@ -0,0 +1,10 @@
+//  Greet Function
+//  Created by Paul Orton on 2/11/19.
+
+import Foundation
+
+func greet(person name: String, greeting: String) -> String {
+    return greeting + " " + name + "!"
+}
+
+print(greet(person: "Paul", greeting: "Hello"))
