1fd5280977557d712743ad4be38c65922108dd78
Paul Orton <ortonpaul18@gmail.com>
2019-02-11 12:23:47 -0600

    Completed convert function assignment

--- Swift Functions/main.swift
+++ Swift Functions/main.swift
@@ -3,8 +3,16 @@
 
 import Foundation
 
+//Assignment 1: Greet Function
 func greet(person name: String, greeting: String) -> String {
     return greeting + " " + name + "!"
 }
 
 print(greet(person: "Paul", greeting: "Hello"))
+
+//Assignment 2: Convert Miles to Kilometers Function
+func convertMilesToKilometers(miles: Double) -> Double {
+    return miles * 1.60934
+}
+
+print("3 miles is \(convertMilesToKilometers(miles: 3)) kilometers.")
